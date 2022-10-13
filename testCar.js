b5cd4b9e1445491e4e1328bb0b6bfa5370853c84
tuan <rickytuan1991@gmail.com>
2022-10-13 13:00:56 +0300

    done

--- testCar.js
+++ testCar.js
@@ -4,4 +4,8 @@ const carData = require('./car.json')
 
 
 console.log('################# #################')
-console.log(carData)
+console.log(carData[0])
+console.log(carData[0].model)
+
+console.log('################# last car #################')
+console.log(carData[carData.length-1])
