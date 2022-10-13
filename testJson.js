66b8909a8e93e934091842777048e355eb484195
tuan <rickytuan1991@gmail.com>
2022-10-13 12:34:55 +0300

    done

--- testJson.js
+++ testJson.js
@@ -1,2 +1,28 @@
+"use strict";
 const data = require('./person.json')
 console.log(data)
+
+
+
+console.log(data['age'])
+
+
+let fieldName = 'age';
+console.log(data[fieldName])
+
+
+function print(fieldName) {
+    return data[fieldName]
+}
+
+
+console.log(print('age'))
+
+
+
+console.log("################## key ####################")
+console.log(Object.keys(data))
+
+for (const key of Object.keys(data)) {
+    console.log(print(key))
+}
