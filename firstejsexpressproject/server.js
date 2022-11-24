f23d1234480ef2f6b48231ccef4b644064e09fe1
tuan <rickytuan1991@gmail.com>
2022-11-24 13:44:58 +0200

    set up ejs

--- firstejsexpressproject/server.js
+++ firstejsexpressproject/server.js
@@ -4,7 +4,10 @@ const path = require('path')
 require('dotenv').config()
 const { port, host } = require('./config.json')
 // so use static to allow express to access public --> that now can allow home.html with link css is style/styles.css will be active
-
+// set up view engin 
+app.set('view engine', 'ejs')
+// where to set the pages
+app.set('views', path.join(__dirname, 'pageTemplates'))
 
 const homePath = path.join(__dirname, 'home.html')
 
@@ -14,8 +17,9 @@ app.get('/', (req, res, next) => {
 })
 // 
 app.post('/login', express.urlencoded({ extended: false }), (req, res) => {
-    const { username,password} = req.body
-    res.status(200).send(`<h1>body ${username}</h1>`)
+    const { username, password } = req.body
+    // res.status(200).send(`<h1>body ${username}</h1>`)
+    res.status(200).render('result', { title: "data", header1: 'your data', data: req.body })
 })
 app.listen(port, host, () => {
     console.log(`listening at port ${port}`)
