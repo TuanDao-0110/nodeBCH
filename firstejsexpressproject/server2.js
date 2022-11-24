f582e9ca9c5a381ceddc3f43869af5de3315c053
tuan <rickytuan1991@gmail.com>
2022-11-24 14:14:36 +0200

    add all user

--- firstejsexpressproject/server2.js
+++ firstejsexpressproject/server2.js
@@ -4,11 +4,17 @@ const path = require('path')
 require('dotenv').config()
 const { port, host } = require('./config.json')
 // so use static to allow express to access public --> that now can allow home.html with link css is style/styles.css will be active
-// set up view engin 
+// set up view engine
 app.set('view engine', 'ejs')
 // where to set the pages
 app.set('views', path.join(__dirname, 'pageTemplates'))
-
+// user 
+const users = {
+    matt: 'secret',
+    vera: '12345',
+    jesse: '333',
+    tuan: 'tuan'
+}
 const homePath = path.join(__dirname, 'home.html')
 
 
@@ -18,12 +24,17 @@ app.get('/', (req, res, next) => {
 // 
 app.post('/login', express.urlencoded({ extended: false }), (req, res) => {
     const { username, password } = req.body
-    // res.status(200).send(`<h1>body ${username}</h1>`)
-    // we can write like this
-    // res.status(200).render('result', { title: "data", header1: 'your data', data: req.body })
-    // 2nd way to write
-    res.status(200).render('result', { title: "data", header1: 'your data', data: { username, password } })
+    // check data first that make sure the exist in our database
+    if (Object.keys(users).includes(username) && users[username] === password) {
+        return res.status(200).render('result', { title: "data", header1: 'your data', data: { username, password } })
+    }
+    return res.status(401).render('error', { username })
 
+
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).render('users', { title: 'username', header1: 'names', usernames: Object.keys(users) })
 })
 app.listen(port, host, () => {
     console.log(`listening at port ${port}`)
