4bcee1852194c8d2746b6fd3be066a6595109fbc
tuan <rickytuan1991@gmail.com>
2022-12-01 13:43:51 +0200

    update code status

---
+++ employe/storage/datastorageLayer.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const { removeFromStorage, updateStorage, addToStorage, getAllFromStorage, getFromStorage } = require('./storageLayer')
+const {CODES,MESSAGES}  =require('./statusCode')
+
+
