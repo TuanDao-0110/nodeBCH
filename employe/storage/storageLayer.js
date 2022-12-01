6f0f8e17f400406b1b352d918a8dbcdb59725b11
tuan <rickytuan1991@gmail.com>
2022-12-01 11:58:09 +0200

    add

--- employe/storage/storageLayer.js
+++ employe/storage/storageLayer.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const path = require('path')
+const { storageFile } = require('./storageConfig.json')
+// get writestorage , read storage
+const { writeStorage, readStorage } = require('./rederWriter')
+// join the filePath to locate our file 
+const storageFilePath = path.join(__dirname, storageFile)
+
+
+const getAllFromStorage = async () => {
+    try {
+        const data = await readStorage(storageFilePath)
+        // read all file from employee.json
+        console.log(data)
+        return data
+    } catch (error) {
+        console.log(error)
+        return []
+    }
+}
+getAllFromStorage()
