c9c47cd7b9e0bea155b5246df9fd3d0d34555810
tuan <rickytuan1991@gmail.com>
2022-12-01 15:09:05 +0200

    new file

--- employe/server.js
+++ employe/server.js
@@ -6,26 +6,42 @@ const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || '4000'
 const { storage } = require('./serverConfig.json')
 const Datastorage = require(path.join(__dirname, storage.storageFolder, storage.dataLayer))
-const newData = new Datastorage()
+const newData = new Datastorage
 
 
-newData.insert({
-    "id": 1,
-    "firstname": "new name",
-    "lastname": "last name",
-    "department": "it",
-    "salary": 4000
-}).then().catch(err => console.log(err))
+// newData.insert({
+//     "id": 1,
+//     "firstname": "new name",
+//     "lastname": "last name",
+//     "department": "it",
+//     "salary": 4000
+// }).then().catch(err => console.log(err))
 
 
 
-// add middleware 
+// add middleware for ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'pages'))
+// add middeware for ...
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+// add menu path 
+const menuPath = path.join(__dirname, 'menu.html')
+// add server 
 app.get('/', (req, res, next) => {
-    res.status(200).json({ mgs: 'success' })
-}).listen(port, host, () => {
+    res.status(200).sendFile(menuPath)
+})
+app.get('/getall', (req, res, next) => {
+
+    newData.getAll().then(data =>
+        res.status(200).render('alldata', { result: data })
+    )
+})
+app.get('/getPerson', (req, res, next) => {
+    res.status(200).render('getPerson', { title: 'get person', header1: 'new header', action: 'userid' })
+})
+
+
+app.listen(port, host, () => {
     console.log(`listening at port ${port}...`)
 })
