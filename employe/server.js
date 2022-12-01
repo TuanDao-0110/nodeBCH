ff3fecc7c8dcd9949feefcdc5380a78116e09144
tuan <rickytuan1991@gmail.com>
2022-12-01 14:37:33 +0200

    add middleware

--- employe/server.js
+++ employe/server.js
@@ -4,7 +4,26 @@ const path = require('path')
 require('dotenv').config
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || '4000'
+const { storage } = require('./serverConfig.json')
+const Datastorage = require(path.join(__dirname, storage.storageFolder, storage.dataLayer))
+const newData = new Datastorage()
 
+
+newData.insert({
+    "id": 1,
+    "firstname": "new name",
+    "lastname": "last name",
+    "department": "it",
+    "salary": 4000
+}).then().catch(err => console.log(err))
+
+
+
+// add middleware 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'pages'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res, next) => {
     res.status(200).json({ mgs: 'success' })
 }).listen(port, host, () => {
--- employe/storage/datastorageLayer.js
+++ employe/storage/datastorageLayer.js
@@ -1,6 +1,77 @@
 'use strict'
 
 const { removeFromStorage, updateStorage, addToStorage, getAllFromStorage, getFromStorage } = require('./storageLayer')
-const {CODES,MESSAGES}  =require('./statusCode')
+const { CODES, MESSAGES } = require('./statusCode')
+const e = require('express')
 
 
+
+
+// data storage class
+module.exports = class Datastorage {
+    get CODES() {
+        return CODES
+    }
+    getAll() {
+        return getAllFromStorage()
+    }// end getAll
+    getOne(id) {
+        return new Promise(async (res, rej) => {
+            // check it exist
+            if (!id) {
+                rej(MESSAGES.NOT_FOUND('----empty----'))
+            } else {
+                const result = await getFromStorage(id)
+                if (result) {
+                    res(result)
+                } else {
+                    rej(MESSAGES.NOT_FOUND(id))
+                }
+            }
+        })
+    }
+    insert(employee) {
+        return new Promise(async (res, rej) => {
+            if (!employee) {
+                rej(MESSAGES.NOT_INSERTED())
+            } else {
+                if (!employee.id) {
+                    rej(MESSAGES.NOT_INSERTED())
+                } else if (await getFromStorage(employee.id)) {
+                    rej(MESSAGES.ALREADY_IN_USE(employee.id))
+                }
+                else if (await addToStorage(employee)) {
+                    res(MESSAGES.INSERT_OK(employee.id))
+                }
+                else {
+                    rej(MESSAGES.NOT_INSERTED())
+                }
+
+            }
+        })
+    }
+    update(employee) {
+        return new Promise(async (res, rej) => {
+            if (!employee) {
+                rej(MESSAGES.NOT_UPDATED())
+            } else {
+                if (await updateStorage(employee)) {
+                    res(MESSAGES.UPDATE_OK(employee))
+                } else {
+                    rej(MESSAGES.NOT_UPDATED())
+                }
+            }
+        })
+    }
+    remove(id) {
+        return new Promise(async (res, rej) => {
+            if (!id) {
+                rej(MESSAGES.NOT_FOUND('---empty---'))
+            } else if (await removeFromStorage(id)) {
+                res(MESSAGES.REMOVE_OK(id))
+            } else {
+                rej(MESSAGES.NOT_REMOVED(id))
+            }
+        })
+    }
+}
