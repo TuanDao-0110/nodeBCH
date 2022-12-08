65898ec0dad02db3ca4bb5c5c6f927a8fac0b8d3
tuan <rickytuan1991@gmail.com>
2022-12-09 00:27:57 +0200

    complete status

--- task/server.js
+++ task/server.js
@@ -22,7 +22,10 @@ app.get('/getall', (req, res, next) => {
         res.render('getall', {
             data, title: 'All Book'
         })
-    ).catch(err => res.send('error'))
+    ).catch(err =>
+        failBanner(res, err.message)
+
+    )
 })
 app.get('/getbook', (req, res, next) => {
     res.render('getbook', { title: 'get book', action: '/getbook', method: 'post' })
@@ -31,15 +34,23 @@ app.post('/getbook', (req, res, next) => {
     const { bookID } = req.body
     newData.getBook(Number(bookID)).then(result =>
         res.render('bookDetails', { result })
-    ).catch(err => res.send(err))
+    ).catch(err =>
+        failBanner(res, err.message)
+
+    )
 })
 app.get('/insertbook', (req, res, next) => {
     res.render('insertbook', { title: 'insert book', action: '/insertbook', })
 })
 app.post('/insertbook', (req, res) => {
     newData.insertBook(req.body).then(data =>
-        res.json({ data })
-    ).catch(err => res.send(err))
+        // res.json({ data })
+        successBanner(res, 'new book added')
+
+    ).catch(err =>
+        failBanner(res, err.message)
+
+    )
 })
 app.get('/findbook', (req, res) => {
     res.render('getbook', { title: 'findbook', action: '/editbook', method: 'get' })
@@ -48,21 +59,47 @@ app.get('/editbook', (req, res) => {
     const { bookID } = req.query
     newData.getBook(Number(bookID)).then(result =>
         res.render('editbook', { result, title: 'edit book', action: '/editbook', method: 'post' })
-    ).catch(err => res.send(err))
+    ).catch(err =>
+        failBanner(res, err.message)
+
+    )
 })
 app.post('/editbook', (req, res) => {
-    newData.editBook(req.body).then(data =>
-        res.json({ data })
-    ).catch(err => res.json({ err }))
+    newData.editBook(req.body).then(result =>
+        successBanner(res, result.message)
+
+    ).catch(err =>
+
+        failBanner(res, err.message)
+
+    )
 })
 app.get('/deletebook', (req, res) => {
     res.render('getbook', { title: 'delete book', action: '/deletebook', method: 'post' })
 })
 app.post('/deletebook', (req, res) => {
     const { bookID } = req.body
-    newData.deleteBook(bookID).then(result => res.json({ result })).catch(err => res.json([err]))
+    newData.deleteBook(bookID).then(result =>
+        // res.json({ result })
+        // res.render('banner', { title: 'success', msg: result.message })
+        successBanner(res, result.message)
+    )
+        .catch(err =>
+            // res.json([err])
+            failBanner(res, err.message)
+
+        )
 
 })
 app.listen(port, host, () => {
     console.log(`listening on port ${port}...`)
 })
+
+const successBanner = (res, msg) => {
+    return res.render('banner', { title: 'success', msg })
+}
+
+const failBanner = (res, msg) => {
+    return res.render('banner', { title: 'fail', msg })
+
+}
